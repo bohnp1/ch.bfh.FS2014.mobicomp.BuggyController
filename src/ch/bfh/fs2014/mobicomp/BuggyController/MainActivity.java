ab06ee2efe71cd4f161fbdf1990383ba009c74bc
Roger Jaggi <roger.jaggi@blastbeatsyndicate.com>
2014-04-11 11:00:19 +0200

    cleanup

--- src/ch/bfh/fs2014/mobicomp/BuggyController/MainActivity.java
+++ src/ch/bfh/fs2014/mobicomp/BuggyController/MainActivity.java
@@ -16,22 +16,54 @@ public class MainActivity extends Activity {
 
     private static final String HOST = "192.168.43.224";
     private static final int PORT = 4223;
-    private static final String UID = "aetiNB3mX2u"; // Change to your UID
 
     private static IPConnection ipcon = new IPConnection(); // Create IP connection
-    private static BrickDC dc = new BrickDC(UID, ipcon); // Create device object
+    private static BrickDC dcLeft = new BrickDC("6wW1Yj", ipcon); // Create device object
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        final Button button = (Button) findViewById(R.id.buttonUp);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button buttonConnect = (Button) findViewById(R.id.buttonConnect);
+        buttonConnect.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    ipcon.connect(HOST, PORT); // Connect to brickd
+
+                    // Don't use device before ipcon is connected
+
+                    dcLeft.setPWMFrequency(10000); // Use PWM frequency of 10kHz
+
+                    dcLeft.setDriveMode((short) 1); // Use 1 = Drive/Coast instead of 0 = Drive/Brake
+
+                    dcLeft.enable();
+                    dcLeft.setAcceleration(5000); // Slow acceleration
+                    dcLeft.setVelocity((short)32767); // Full speed forward
+
+
+                    final TextView textViewLog = (TextView) findViewById(R.id.textViewLog);
+                    textViewLog.setText("connected =)");
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (AlreadyConnectedException e) {
+                    e.printStackTrace();
+                } catch (TimeoutException e) {
+                    e.printStackTrace();
+                } catch (NotConnectedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        final Button buttonUp = (Button) findViewById(R.id.buttonUp);
+        buttonUp.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
             }
         });
+
 
         //final TinkerforgeApplication the17HerzApplication = TinkerforgeApplication.getInstance(); // new The17HerzApplication();
 
@@ -39,32 +71,7 @@ public class MainActivity extends Activity {
         //TinkerforgeStackAgency.getInstance().getStackAgent(TinkerforgeApplication.BARO_SENSOR).addApplication(the17HerzApplication);
 
 
-        try {
-            ipcon.connect(HOST, PORT); // Connect to brickd
 
-        // Don't use device before ipcon is connected
-
-            dc.setPWMFrequency(10000); // Use PWM frequency of 10kHz
-
-            dc.setDriveMode((short) 1); // Use 1 = Drive/Coast instead of 0 = Drive/Brake
-
-            dc.enable();
-            dc.setAcceleration(5000); // Slow acceleration
-            dc.setVelocity((short)32767); // Full speed forward
-
-
-            final TextView textViewLog = (TextView) findViewById(R.id.textViewLog);
-            textViewLog.setText("connected =)");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (AlreadyConnectedException e) {
-            e.printStackTrace();
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-        } catch (NotConnectedException e) {
-            e.printStackTrace();
-        }
 
         //System.console().readLine("Press key to exit\n");
 
